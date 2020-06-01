6d3efb0550a0365641f46d75590449c1f13d7956
xtreme-uz <rustambekov.mail.box@gmail.com>
2020-06-02 00:31:03 +0500

    Initial commit

---
+++ src/main/java/uz/xtreme/bot/BotApplication.java
@@ -0,0 +1,27 @@
+package uz.xtreme.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@SpringBootApplication
+public class BotApplication {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new ChkoffBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        SpringApplication.run(BotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/xtreme/bot/ChkoffBot.java
@@ -0,0 +1,40 @@
+package uz.xtreme.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Author: Rustambekov Avazbek
+ * Date: 01/06/2020
+ * Time: 01:28
+ */
+public class ChkoffBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "ChkoffBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1001664336:AAHhxf9N3p_PVhEoM2fGKDNOk0Y5zA5mm1c";
+    }
+}
